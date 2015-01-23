a712d0f63c5eba3d23beda54f7b1077601fb567b
Zillah Chen <zillah_c@hotmail.com>
2015-01-22 17:02:24 -0800

    author_name presence required test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
   validates :title, presence:true
+  validates :author_name, presence:true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe Book, :type => :model do
     book = FactoryGirl.build(:book, title:nil)
     expect(book).to be_invalid
   end
-  
-  it "invalid without author_name"
+
+  it "invalid without author_name" do
+    book = FactoryGirl.build(:book, author_name:nil)
+    expect(book).to be_invalid
+  end 
   it "has valid factory"
   it "should have an integer as page number"
 
