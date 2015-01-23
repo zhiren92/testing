e9cf25c84518f81bd60962a0de9f659d93fa3204
Zillah Chen <zillah_c@hotmail.com>
2015-01-22 16:57:30 -0800

    add test for title required

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :book do
-    title "MyString"
-author_name "MyString"
-subject "MyString"
-genre "MyString"
-summary "MyString"
-pagecount "MyString"
+    title "Book"
+    author_name "Testing"
+    subject "Testing"
+    genre "MyString"
+    summary "MyString"
+    pagecount 50
   end
 
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Book, :type => :model do
-  it "invalid without title"
+
+  it "invalid without title"  do
+    book = FactoryGirl.build(:book, title:nil)
+    expect(book).to be_invalid
+  end
+  
   it "invalid without author_name"
   it "has valid factory"
+  it "should have an integer as page number"
+
 end
