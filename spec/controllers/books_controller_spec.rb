1fc394e8d4ec0dfa0b7c0fe2ebcd2b0441ecec2b
Zillah Chen <zillah_c@hotmail.com>
2015-01-22 17:30:18 -0800

    created controller test for index

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, :type => :controller do
-  it "responds successfully with an HTTP 200 status code" 
-  it "it renders the index template"
-  it "responds successfully with an HTTP 200 status code"
-  it 'renders the index template'
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+    it "it renders the index template"
+    it "responds successfully with an HTTP 200 status code"
+    it 'renders the index template'
 
+  end
 
 end
