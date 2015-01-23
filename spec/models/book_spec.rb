0da2a6cae868c64a0cd1f13b1bced7fe8c99b70e
Zillah Chen <zillah_c@hotmail.com>
2015-01-22 17:09:49 -0800

    legit factory

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Book, :type => :model do
 
+  it "has valid factory" do
+    expect(FactoryGirl.build(:book)).to be_valid  
+  end
+
   it "invalid without title"  do
     book = FactoryGirl.build(:book, title:nil)
     expect(book).to be_invalid
   end
-  
-  it "invalid without author_name"
-  it "has valid factory"
+
+  it "invalid without author_name" do
+    book = FactoryGirl.build(:book, author_name:nil)
+    expect(book).to be_invalid
+  end 
+
+
   it "should have an integer as page number"
 
 end
